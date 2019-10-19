6754560e3cfefb43e25e5168e905f8e2e7821561
Aaron <10217842+aaron-sonin@users.noreply.github.com>
2019-10-19 00:12:37 -0600

    Update .gitignore

--- CMakeFiles/3.15.4/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aaron/iobots/CMakeFiles/3.15.4/CompilerIdC/CMakeCCompilerId.c
--- CMakeFiles/3.15.4/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aaron/iobots/CMakeFiles/3.15.4/CompilerIdCXX/CMakeCXXCompilerId.cpp
